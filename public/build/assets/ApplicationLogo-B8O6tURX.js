81f1c54a72d4eb270dcbcf008476974de3197210
darwin ibay <darwin.i@shoredigitalinc.com>
2024-12-20 22:17:01 +0800

    updates

---
+++ public/build/assets/ApplicationLogo-B8O6tURX.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-B3q1B4vg.js";function i(o){return r.jsx("img",{src:"logo.png",...o})}export{i as A};
--- public/build/assets/ApplicationLogo-Bn7Ec_-U.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-D2vf_10L.js";function i(o){return r.jsx("img",{src:"logo.png",...o})}export{i as A};
