863f22a2b1c4f057a9d135bb42d2081216cdeefe
darwin ibay <darwin.i@shoredigitalinc.com>
2024-12-20 15:14:26 +0800

    updaates

--- app/Http/Controllers/TemperatureController.php
+++ app/Http/Controllers/TemperatureController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TempHistory;
 use Illuminate\Http\Request;
 
 class TemperatureController extends Controller
 {
     public function store(Request $request)
     {
-        $temperature = $request->input('temperature');
+        $temp1 = $request->input('temp1');
+        $temp2 = $request->input('temp2');
+
+        TempHistory::create(['fishpond_id' => 1,'temperature'=> $temp1]);
+        TempHistory::create(['fishpond_id' => 2,'temperature'=> $temp2]);
 
         // Save the temperature data to the database or perform any other necessary actions
 
